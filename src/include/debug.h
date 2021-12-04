d2ef08693c2b3ff2be34e5cfc95d2aec466e2a95
Kevin Douchi <93414678+KevinDch@users.noreply.github.com>
2021-12-04 11:41:48 +0800

    Update debug.h

--- src/include/debug.h
+++ src/include/debug.h
@@ -1,5 +1,5 @@
-#ifndef MSGMAP_ERROR_H
-#define MSGMAP_ERROR_H
+#ifndef C51_EMU_DEBUG_H
+#define C51_EMU_DEBUG_H
 
 /** @file
  *
@@ -14,10 +14,6 @@
 #include <sys/sysinfo.h>
 #include <string>
 
-/// current executable pathname, filled by main()
-/// if not filled, addr2line in OBTAIN_STACK_FRAME will not be available
-extern const char * __path_to_executable;
-
 /// check if addr2line command is available, filled by __check_addr2line()
 extern bool __is_addr2line_available;
 
@@ -25,7 +21,6 @@ extern bool __is_addr2line_available;
 extern bool __is_time_enabled;
 
 /// check addr2line availability
-/// you have to run this before you can use addr2line in OBTAIN_STACK_FRAME
 void __check_addr2line();
 
 /// execute a command and get its output
@@ -38,6 +33,11 @@ std::string __exec_cmd(const std::string& cmd);
 /// @return address of stack frame
 std::string __get_addr(const std::string& input);
 
+/// get pathname of elf file in stack frame output
+/// @param input raw stack frame input
+/// @return pathname of elf file
+std::string __get_path(const std::string& input);
+
 /// clean addr2line output, remove "?? ??:0"
 /// @param input addr2line raw output
 /// @return useful information provided by addr2line, or "(no specific information)" if not
@@ -47,6 +47,12 @@ std::string __clean_addr2line_output(const std::string& input);
 /// @param none
 /// @return std::string of current time
 std::string __current_time();
+
+static class __exec_check_addr2line__
+{
+public:
+    __exec_check_addr2line__() noexcept { __check_addr2line(); }
+} __init_check_addr2line__;
 
 # define BACKTRACE_SZ 64    /* Max stack frame size */
 /// Automatically obtain stack frame
@@ -70,11 +76,11 @@ std::string __current_time();
         for (__i = 0; __i < __size; __i++)              \
         {                                               \
             std::cerr << "\t" << __strings[__i];        \
-            if (__path_to_executable && __is_addr2line_available)                       \
+            if (__is_addr2line_available)               \
             {                                                                           \
                 std::cerr << " " << __clean_addr2line_output(                           \
                                     __exec_cmd(std::string("addr2line a -f -p -e ")     \
-                                        + __path_to_executable + " "                    \
+                                        + __get_path(__strings[__i]) + " "              \
                                         + __get_addr(__strings[__i])                    \
                                     )                   \
                                 );                      \
@@ -103,4 +109,4 @@ std::string __current_time();
 
 #endif // CMAKE_BUILD_DEBUG
 
-#endif //MSGMAP_ERROR_H
+#endif //C51_EMU_DEBUG_H
